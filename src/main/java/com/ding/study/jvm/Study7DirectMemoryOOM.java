c37afb84fc040801a040f352a884023e36b13070
saiding@creditease.cn <saiding@creditease.cn>
2018-01-22 10:41:13 +0800

    JVM复习 方法区溢出6和7的区别

---
+++ src/main/java/com/ding/study/jvm/Study7DirectMemoryOOM.java
@@ -0,0 +1,25 @@
+package com.ding.study.jvm;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**直接内存direct
+ * VM args:-Xmx2M -XX:MaxDirectMemorySize=1M
+ * @author daniel 2018-1-8 0008.
+ */
+public class Study7DirectMemoryOOM {
+
+    private static final int _1BM=1024*1024;
+    public static void main(String[] args) throws Throwable {
+
+        Field unsafeField= Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe=(Unsafe)unsafeField.get(null);
+        while (true){
+            unsafe.allocateMemory(_1BM);
+        }
+
+
+    }
+}
