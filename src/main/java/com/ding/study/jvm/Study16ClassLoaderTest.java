85fc3b0acbb391802200d8a1a718bdfb5669a9e8
saiding@creditease.cn <saiding@creditease.cn>
2018-05-28 17:31:47 +0800

    JVM 命令jinfo 查看jvm配置和system.getpropertiese信息

--- src/main/java/com/ding/study/jvm/Study16ClassLoaderTest.java
+++ src/main/java/com/ding/study/jvm/Study16ClassLoaderTest.java
@@ -3,16 +3,23 @@ package com.ding.study.jvm;
 import java.io.InputStream;
 
 /**
+ * p229
+ * 输出
+ * class com.ding.study.jvm.Study16ClassLoaderTest
+ false
+ 第二句返回false虚拟机存在了两个classloadertest 一个是系统应用程序类加载器加载，另外一个是自定义加载器加载。
+ 所以instanceof 比对为false
+ *
  * @author daniel 2018-5-23 0023.
  */
 public class Study16ClassLoaderTest {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 
         ClassLoader myLoader = new ClassLoader() {
             @Override
             public Class<?> loadClass(String name) throws ClassNotFoundException {
-
                 try {
                     String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
                     InputStream is = getClass().getResourceAsStream(fileName);
@@ -23,16 +30,17 @@ public class Study16ClassLoaderTest {
                     byte[] b = new byte[is.available()];
                     is.read(b);
                     return defineClass(name, b, 0, b.length);
-
-
                 } catch (Exception e) {
-
+                    throw new ClassNotFoundException(name);
                 }
-
-
-                return super.loadClass(name);
             }
         };
+
+        Object obj = myLoader.loadClass("com.ding.study.jvm.Study16ClassLoaderTest").newInstance();
+
+        System.out.println(obj.getClass());
+        System.out.println(obj instanceof com.ding.study.jvm.Study16ClassLoaderTest);
+
 
     }
 
