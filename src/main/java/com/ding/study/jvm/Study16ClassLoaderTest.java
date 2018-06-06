95cc6035760d1f17cd328431e9fc9f2925bf0d7d
saiding@creditease.cn <saiding@creditease.cn>
2018-06-06 18:10:05 +0800

    JVM 新增类加载

--- src/main/java/com/ding/study/jvm/Study16ClassLoaderTest.java
+++ src/main/java/com/ding/study/jvm/Study16ClassLoaderTest.java
@@ -1,8 +1,18 @@
 package com.ding.study.jvm;
 
+import sun.security.ec.NamedCurve;
+
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 /**
+ *
+ * 通过一个类的全限定名字来获取描述此类的二进制字节流；虚拟机外部实现，让程序自己决定如何去获取所需要的类。
+ *
+ * 虚拟机类的唯一性=类加载器+类本身；
+ *
+ *
  * p229
  * 输出
  * class com.ding.study.jvm.Study16ClassLoaderTest
@@ -41,8 +51,46 @@ public class Study16ClassLoaderTest {
         System.out.println(obj.getClass());
         System.out.println(obj instanceof com.ding.study.jvm.Study16ClassLoaderTest);
 
+        System.out.println(Study16ClassLoaderTest.class.getClassLoader());
+        System.out.println(obj.getClass().getClassLoader());
+        System.out.println(String.class.getClassLoader());
+        System.out.println(NamedCurve.class.getClassLoader());
 
+
+        System.out.println("BootstrapClassLoader 的加载路径: ");
+
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for(URL url : urls) {
+            System.out.println(url);
+        }
+        System.out.println("----------------------------");
+
+        //取得扩展类加载器
+        URLClassLoader extClassLoader = (URLClassLoader)ClassLoader.getSystemClassLoader().getParent();
+
+        System.out.println(extClassLoader);
+        System.out.println("扩展类加载器 的加载路径: ");
+
+        urls = extClassLoader.getURLs();
+        for(URL url : urls) {
+            System.out.println(url);
+        }
+        System.out.println("----------------------------");
+
+
+        //取得应用(系统)类加载器
+        URLClassLoader appClassLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();
+
+        System.out.println(appClassLoader);
+        System.out.println("应用(系统)类加载器 的加载路径: ");
+
+        urls = appClassLoader.getURLs();
+        for(URL url : urls) {
+            System.out.println(url);
+        }
+        System.out.println("----------------------------");
     }
+
 
 
 }
