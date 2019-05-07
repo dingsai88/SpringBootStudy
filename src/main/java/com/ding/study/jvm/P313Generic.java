692ce2ad83a2c177f670cc9fdadd1a4cdc23652d
saiding@creditease.cn <saiding@creditease.cn>
2019-05-07 14:56:54 +0800

    由于是伪泛型，所以方法不能编译。

---
+++ src/main/java/com/ding/study/jvm/P313Generic.java
@@ -0,0 +1,17 @@
+package com.ding.study.jvm;
+
+import java.util.List;
+
+/**
+ * @author daniel 2019-5-7 0007.
+ */
+public class P313Generic {
+
+/*    public static void test(List<Integer> i){
+        System.out.println("integer");
+    }*/
+
+    public static void test(List<String> i){
+        System.out.println("String");
+    }
+}
