db244dd2bf4fa578627e6cf45c992ee04dd916a8
saiding@creditease.cn <saiding@creditease.cn>
2019-06-06 11:24:32 +0800

    引用计数算法 reference counting 和idea GC打印 -XX:+PrintGCDetails

--- src/main/java/com/ding/study/jvm/Study8T3ReferenceCountingGC.java
+++ src/main/java/com/ding/study/jvm/Study8T3ReferenceCountingGC.java
@@ -1,8 +1,9 @@
 package com.ding.study.jvm;
 
 /**
+ idea增加GC vm Options 增加参数: -XX:+PrintGCDetails
  * @author daniel 2018-1-22 0022.
- * 依然会被回收。page63
+ *         依然会被回收。page63
  */
 public class Study8T3ReferenceCountingGC {
 
@@ -11,10 +12,9 @@ public class Study8T3ReferenceCountingGC {
     private byte[] bigSize = new byte[2 * _1BM];
 
     /**
-     *
      * @param args
      */
-    public static void main(String [] args) {
+    public static void main(String[] args)throws Exception {
         Study8T3ReferenceCountingGC objA = new Study8T3ReferenceCountingGC();
         Study8T3ReferenceCountingGC objB = new Study8T3ReferenceCountingGC();
         objA.instance = objB;
@@ -25,6 +25,7 @@ public class Study8T3ReferenceCountingGC {
         System.gc();
 
 
+        Thread.sleep(111);
     }
 
 }
