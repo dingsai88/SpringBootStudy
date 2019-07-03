ad83c78a1f515137f19c5b3f1f02d2580081a7e6
saiding@creditease.cn <saiding@creditease.cn>
2019-07-03 10:21:45 +0800

    collection和map

---
+++ src/main/java/com/ding/study/util/TestMain.java
@@ -0,0 +1,41 @@
+package com.ding.study.util;
+
+import org.codehaus.groovy.util.StringUtil;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.UUID;
+
+/**
+ * @author daniel 2019-6-27 0027.
+ */
+public class TestMain {
+
+
+    public static void main(String[] args) throws Exception {
+        String str="66_";
+        SimpleDateFormat simdf = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        System.out.println("现在时间："+simdf.format(Calendar.getInstance().getTime()));
+        String str2="66_1906271056_123456789012345678";
+        System.out.println(str2.length());
+        System.out.println(UUID.randomUUID());
+        System.out.println(getRequestId("123456789012345678").length());
+        System.out.println(getRequestId("123456789012345678"));
+    }
+
+    /**
+     *获取请求支付中心的请求ID
+     * @param id GUARANTEE_LOAN_RECORD的自增ID
+     * @return 66_yyMMddHHmm_表id
+     */
+    public static String getRequestId(String id){
+        if(StringUtils.isEmpty(id)){
+            throw new RuntimeException("表自增ID为空");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmm");
+        return "66_"+sdf.format(Calendar.getInstance().getTime())+"_"+id;
+    }
+
+}
