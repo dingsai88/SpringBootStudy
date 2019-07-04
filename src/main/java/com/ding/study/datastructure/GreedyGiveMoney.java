370d6b6987f467e686ca996b881c84838187da9a
saiding@creditease.cn <saiding@creditease.cn>
2019-07-04 18:37:52 +0800

    基础复习:数据结构

---
+++ src/main/java/com/ding/study/datastructure/GreedyGiveMoney.java
@@ -0,0 +1,27 @@
+package com.ding.study.datastructure;
+
+/**
+ * @author daniel 2019-7-4 0004.
+ */
+public class GreedyGiveMoney {
+
+
+
+    public static void main(String[] args) {
+        greedyGiveMoney(12);
+    }
+
+    public static void greedyGiveMoney(int money) {
+        System.out.println("需要找零: " + money);
+        int[] moneyLevel = {1, 5, 10, 20, 50, 100};
+        for (int i = moneyLevel.length - 1; i >= 0; i--) {
+            int num = money/ moneyLevel[i];
+            int mod = money % moneyLevel[i];
+            money = mod;
+            if (num > 0) {
+                System.out.println("需要" + num + "张" + moneyLevel[i] + "块的");
+            }
+        }
+    }
+
+}
