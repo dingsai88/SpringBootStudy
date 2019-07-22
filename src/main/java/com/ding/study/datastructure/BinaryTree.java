5c578222007c5a33392c329360f14cb3dd3117dd
saiding@creditease.cn <saiding@creditease.cn>
2019-07-22 10:52:29 +0800

    基础复习:数据结构-树开始

---
+++ src/main/java/com/ding/study/datastructure/BinaryTree.java
@@ -0,0 +1,35 @@
+package com.ding.study.datastructure;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.*;
+
+/**
+ * @author daniel 2019-7-17 0017.
+ */
+public class BinaryTree {
+    public static void main(String [] args){
+        Collection collection=new ArrayList();
+        Collection collection1=new HashSet();
+
+        List list=new ArrayList();
+        //被arrsyList 代替
+        List list1=new Vector();
+        List list2=new LinkedList();
+
+        //底层hashMap
+        Set set=new HashSet();
+        //底层linkedHashMap
+        Set set1=new LinkedHashSet();
+        //底层treemap
+        Set set2=new TreeSet();
+
+        Map map=new HashMap();
+        Map map1=new TreeMap();
+        Map map2=new LinkedHashMap();
+        //被hashmap代替
+        Map map3=new Hashtable();
+
+
+
+    }
+}
---
+++ src/main/java/com/ding/study/datastructure/MapTest.java
@@ -0,0 +1,15 @@
+package com.ding.study.datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author daniel 2019-7-19 0019.
+ */
+public class MapTest {
+
+    public static void main(String [] args){
+        HashMap map=new HashMap();
+        map.put("","");
+    }
+}
