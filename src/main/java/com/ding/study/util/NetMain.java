d8d6b926edaf85ffa53d99084a56b3db1da92f10
saiding@creditease.cn <saiding@creditease.cn>
2019-08-30 15:22:10 +0800

    上传两张图片

--- src/main/java/com/ding/study/util/NetMain.java
+++ src/main/java/com/ding/study/util/NetMain.java
@@ -17,42 +17,43 @@ import org.apache.http.util.EntityUtils;
 import java.io.File;
 import java.io.IOException;
 
-/**maven:
- *
- * 	<dependency>
- <groupId>org.apache.httpcomponents</groupId>
- <artifactId>httpcore</artifactId>
- <version>4.4.10</version>
- </dependency>
-
- <dependency>
- <groupId>org.apache.httpcomponents</groupId>
- <artifactId>httpclient</artifactId>
- <version>4.5.6</version>
- </dependency>
- <dependency>
- <groupId>org.apache.httpcomponents</groupId>
- <artifactId>httpmime</artifactId>
- <version>4.5.9</version>
- </dependency>
-
- <dependency>
- <groupId>commons-fileupload</groupId>
- <artifactId>commons-fileupload</artifactId>
- <version>1.2.1</version>
- </dependency>
-
+/**
+ * maven:
+ * <p>
+ * <dependency>
+ * <groupId>org.apache.httpcomponents</groupId>
+ * <artifactId>httpcore</artifactId>
+ * <version>4.4.10</version>
+ * </dependency>
+ * <p>
+ * <dependency>
+ * <groupId>org.apache.httpcomponents</groupId>
+ * <artifactId>httpclient</artifactId>
+ * <version>4.5.6</version>
+ * </dependency>
+ * <dependency>
+ * <groupId>org.apache.httpcomponents</groupId>
+ * <artifactId>httpmime</artifactId>
+ * <version>4.5.9</version>
+ * </dependency>
+ * <p>
+ * <dependency>
+ * <groupId>commons-fileupload</groupId>
+ * <artifactId>commons-fileupload</artifactId>
+ * <version>1.2.1</version>
+ * </dependency>
+ * <p>
  * 上传两张图片
+ *
  * @author daniel 2019-8-30 0030.
  */
 public class NetMain {
     /**
      * 上传两张图片
+     *
      * @return
      */
     public static String decaptcha() {
-
-        CloseableHttpClient client = HttpClients.createDefault();
         //167-http://xx.xx.15.126:8092/app/file/stream/upload
         //155-http://xx.xx.11.91:8080/app/file/stream/upload
         HttpPost post = new HttpPost("http://xx.xx.11.91:8080/app/file/stream/upload");
@@ -69,25 +70,32 @@ public class NetMain {
         // 相当于 <input type="file" class="file" name="file">,匹配@RequestParam("file")
         // .addPart()可以设置模拟浏览器<input/>的表单提交
         builder.addTextBody("userId", "333");
-        builder.addTextBody("idCardNo","11111111111");
-        builder.addTextBody("bankCode","zb");
-        builder.addTextBody("userIp","124.0.0.4");
-        builder.addTextBody("isNew","true");
+        builder.addTextBody("idCardNo", "11111111111");
+        builder.addTextBody("bankCode", "zb");
+        builder.addTextBody("userIp", "124.0.0.4");
+        builder.addTextBody("isNew", "true");
         HttpEntity entity = builder.build();
         post.setEntity(entity);
         String result = "";
-
+        CloseableHttpClient client = HttpClients.createDefault();
         try {
-            CloseableHttpResponse e = client.execute(post);
-            HttpEntity resEntity = e.getEntity();
-            if(entity != null) {
+            CloseableHttpResponse closeableHttpResponse = client.execute(post);
+            HttpEntity resEntity = closeableHttpResponse.getEntity();
+            if (entity != null) {
                 result = EntityUtils.toString(resEntity);
                 System.out.println("response content:" + result);
             }
-        } catch (IOException var10) {
-             var10.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();//关闭httpclient
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
-
         return result;
     }
 
@@ -95,6 +103,6 @@ public class NetMain {
      * @param args
      */
     public static void main(String[] args) {
-        NetMain.decaptcha();
+        System.out.println("最终返回:"+NetMain.decaptcha());
     }
 }
