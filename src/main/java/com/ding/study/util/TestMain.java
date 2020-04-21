3733f26d547cc38958dc4ecb31016c57c5263c7f
saiding@creditease.cn <saiding@creditease.cn>
2020-04-21 19:07:27 +0800

    架构师

--- src/main/java/com/ding/study/util/TestMain.java
+++ src/main/java/com/ding/study/util/TestMain.java
@@ -12,8 +12,14 @@ import java.util.UUID;
  * @author daniel 2019-6-27 0027.
  */
 public class TestMain {
-
     public static void main(String[] args) throws Exception {
+        String App_biz_id="0123456789";
+        String appBizId = App_biz_id.substring(2, App_biz_id.length());
+
+            System.out.println(appBizId);
+
+    }
+    public static void main222(String[] args) throws Exception {
         Objects.requireNonNull(null);
         float j=1;
 
