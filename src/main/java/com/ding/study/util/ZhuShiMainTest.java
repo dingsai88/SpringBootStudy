57d12012116a77abb28fde3e2658c74db533713d
saiding@creditease.cn <saiding@creditease.cn>
2020-05-18 11:40:51 +0800

    unicode注释

---
+++ src/main/java/com/ding/study/util/ZhuShiMainTest.java
@@ -0,0 +1,12 @@
+package com.ding.study.util;
+
+/**
+ * @author daniel 2020-5-18 0018.
+ */
+public class ZhuShiMainTest {
+    public static void main(String[] args) {
+        String name = "沉默王二";
+        // \u000dname="沉默王三";
+        System.out.println(name);
+    }
+}
