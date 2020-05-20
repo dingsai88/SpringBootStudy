aa72783e5152e8328cdd0157ebce24d883c79efe
saiding@creditease.cn <saiding@creditease.cn>
2020-05-20 17:35:04 +0800

    新增一些描述

--- src/main/java/com/ding/study/util/ZhuShiMainTest.java
+++ src/main/java/com/ding/study/util/ZhuShiMainTest.java
@@ -1,6 +1,8 @@
 package com.ding.study.util;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author daniel 2020-5-18 0018.
@@ -10,6 +12,14 @@ public class ZhuShiMainTest {
         String name = "沉默王二";
         // \u000dname="沉默王三";
         System.out.println(name);
+        String a1=new String("age1");
+        String a2=new String("age1");
+        System.out.println("==:"+a1==a2);
+        HashSet<String> set=new HashSet<>();
+        set.add(a1);
+        set.add(a2);
+        System.out.println("==a1:"+a1.hashCode());
+        System.out.println("==a2:"+a2.hashCode());
 
     }
 }
