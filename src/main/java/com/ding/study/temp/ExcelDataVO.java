eb8bdac865e89d3c28ccb3790e7904c5c5a7c39a
saiding@creditease.cn <saiding@creditease.cn>
2020-10-16 16:44:36 +0800

    excel读取生成sql

---
+++ src/main/java/com/ding/study/temp/ExcelDataVO.java
@@ -0,0 +1,116 @@
+package com.ding.study.temp;
+
+/**
+ * @author daniel 2020-10-16 0016.
+ */
+
+public class ExcelDataVO {
+
+    private String data1;
+    private String data2;
+    private String data3;
+    private String data4;
+    private String data5;
+    private String data6;
+    private String data7;
+    private String data8;
+    private String data9;
+    private String data10;
+
+    public String getData1() {
+        return data1;
+    }
+
+    public void setData1(String data1) {
+        this.data1 = data1;
+    }
+
+    public String getData2() {
+        return data2;
+    }
+
+    public void setData2(String data2) {
+        this.data2 = data2;
+    }
+
+    public String getData3() {
+        return data3;
+    }
+
+    public void setData3(String data3) {
+        this.data3 = data3;
+    }
+
+    public String getData4() {
+        return data4;
+    }
+
+    public void setData4(String data4) {
+        this.data4 = data4;
+    }
+
+    public String getData5() {
+        return data5;
+    }
+
+    public void setData5(String data5) {
+        this.data5 = data5;
+    }
+
+    public String getData6() {
+        return data6;
+    }
+
+    public void setData6(String data6) {
+        this.data6 = data6;
+    }
+
+    public String getData7() {
+        return data7;
+    }
+
+    public void setData7(String data7) {
+        this.data7 = data7;
+    }
+
+    public String getData8() {
+        return data8;
+    }
+
+    public void setData8(String data8) {
+        this.data8 = data8;
+    }
+
+    public String getData9() {
+        return data9;
+    }
+
+    public void setData9(String data9) {
+        this.data9 = data9;
+    }
+
+    public String getData10() {
+        return data10;
+    }
+
+    public void setData10(String data10) {
+        this.data10 = data10;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ExcelDataVO{");
+        sb.append("data1='").append(data1).append('\'');
+        sb.append(", data2='").append(data2).append('\'');
+        sb.append(", data3='").append(data3).append('\'');
+        sb.append(", data4='").append(data4).append('\'');
+        sb.append(", data5='").append(data5).append('\'');
+        sb.append(", data6='").append(data6).append('\'');
+        sb.append(", data7='").append(data7).append('\'');
+        sb.append(", data8='").append(data8).append('\'');
+        sb.append(", data9='").append(data9).append('\'');
+        sb.append(", data10='").append(data10).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/ding/study/temp/ExcelTestMain.java
@@ -0,0 +1,72 @@
+package com.ding.study.temp;
+
+import com.ding.study.util.JsonUtils;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author daniel 2020-10-16 0016.
+ */
+public class ExcelTestMain {
+    public static void main(String[] args) {
+        // 设定Excel文件所在路径
+        // String excelFileName = "E:\\JunZhengLimit.xlsx";
+        String excelFileName = "E:\\银行编码.xls";
+        // 读取Excel文件内容
+        List<ExcelDataVO> readResult = ExcelReader.readExcel(excelFileName);
+        System.out.println("最终返回:" + JsonUtils.convertObjToJsonString(readResult));
+
+        //结算的keyICBC; value:{"data1":"工商银行","data2":"ICBC","data3":"0102"}
+        Map<String, ExcelDataVO> jieSuanBankCodeMap = new HashMap<>();
+        for (ExcelDataVO d : readResult) {
+            //key ICBC;
+            jieSuanBankCodeMap.put(d.getData2(), d);
+        }
+
+        String junzhengFileName = "E:\\\\JunZhengLimit.xlsx";
+        // 读取Excel文件内容
+        List<ExcelDataVO> junzhengResult = ExcelReader.readExcel(junzhengFileName);
+        System.out.println("最终返回君正:" + JsonUtils.convertObjToJsonString(junzhengResult));
+
+        int countAll = 0;
+        int countSuccess = 0;
+        for (ExcelDataVO jun : junzhengResult) {
+            countAll++;
+
+            if (jieSuanBankCodeMap.get(jun.getData2()) == null) {
+                continue;
+            }
+            countSuccess++;
+            ExcelDataVO jieSuanBank=   jieSuanBankCodeMap.get(jun.getData2());
+           // System.out.println(jieSuanBank.getData1()+"最终:" + JsonUtils.convertObjToJsonString(jun) + "   " + jieSuanBank);
+            //{"data1":"1","data2":"ICBC","data3":"中国工商银行","data4":"直连普通","data5":"借记卡","data6":"3万","data7":"10万","data8":"50万"}
+            // ExcelDataVO{data1='工商银行', data2='ICBC', data3='0102', data4='null', data5='null', data6='null', data7='null', data8='null', data9='null', data10='null'}
+            System.out.println("INSERT INTO `bank_limit` ( `bank_code`, `bank_name`, `bank_name_abbr`, `limit_bank_code`, `limit_bank_name`, `single_limit`, `date_limit`, `month_limit`, `single_limit_dk`, `single_limit_kj`, `enable_ok`, `create_time`, `update_time`, `source`) VALUES " +
+                    "('1001', '禹州新民生村镇银行', NULL, '"+jieSuanBank.getData3()+"', '"+jieSuanBank.getData1()+"', "+getLimit(jun.getData6())+", "+getLimit(jun.getData7())+", "+getLimit(jun.getData8())+", NULL, NULL, 0, '2020-10-01 10:10:10', '2020-10-01 10:10:10', 2);");
+        }
+
+        System.out.println("最终君正共计银行:" + countAll+"; 找到对应:"+countSuccess);
+
+    }
+
+
+
+    public static BigDecimal getLimit(String data){
+        try {
+            data=data.replaceAll("万","0000");
+            data=data.replaceAll("W","0000");
+            data=data.replaceAll("W","0000");
+            if(data.equals("无限额")){
+                return null;
+            }
+            return new BigDecimal(data);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return BigDecimal.ZERO;
+    }
+}
