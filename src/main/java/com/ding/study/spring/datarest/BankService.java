73a0d5f652e960520c0f01e14fd1d660ba60bdf6
saiding@creditease.cn <saiding@creditease.cn>
2021-02-22 11:18:29 +0800

    Spring data rest

--- src/main/java/com/ding/study/spring/datarest/BankService.java
+++ src/main/java/com/ding/study/spring/datarest/BankService.java
@@ -3,6 +3,7 @@ package com.ding.study.spring.datarest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * I.隐藏类里全部接口、方法、字段：
@@ -11,16 +12,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * 3.隐藏字段:@JsonIgnore
  * <p>
  * I.隐藏-默认的删除方法
- *  @Override
- *   @RestResource(exported = false)
- *   void delete(Long id); *
- *   @Override
- *   @RestResource(exported = false)
- *   void delete(Person entity);
- *
- *
  *
  * @author daniel 2021-2-19 0019.
+ * @Override
+ * @RestResource(exported = false)
+ * void delete(Long id); *
+ * @Override
+ * @RestResource(exported = false)
+ * void delete(Person entity);
  */
 @RepositoryRestResource(path = "bank")
 public interface BankService extends JpaRepository<Bank, Long> {
@@ -32,5 +31,11 @@ public interface BankService extends JpaRepository<Bank, Long> {
      */
     Bank findByBankCode(@Param("bankCode") String key);
 
+    @Override
+    @RestResource(exported = false)
+    void delete(Long id);
 
+    @Override
+    @RestResource(exported = false)
+    void delete(Bank entity);
 }
