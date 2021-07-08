7888d6dcef817ad9be869677470adb9e073cc3d2
saiding@creditease.cn <saiding@creditease.cn>
2021-07-08 10:05:14 +0800

    子数组最大累加和  写错了

--- src/main/java/com/ding/study/nowcoder/a19Array_MaxsumofSubarray/SolutionMaxsumofSubarray.java
+++ src/main/java/com/ding/study/nowcoder/a19Array_MaxsumofSubarray/SolutionMaxsumofSubarray.java
@@ -1,36 +1,30 @@
 package com.ding.study.nowcoder.a19Array_MaxsumofSubarray;
 
 /**
- * https://www.nowcoder.com/practice/554aa508dd5d4fefbf0f86e5fe953abd?tpId=190&&tqId=35386&rp=1&ru=/ta/job-code-high-rd&qru=/ta/job-code-high-rd/question-ranking
+ * NC19 子数组的最大累加和问题
+ * <p>
+ * 描述
+ * 给定一个数组arr，返回子数组的最大累加和
+ * 例如，arr = [1, -2, 3, 5, -2, 6, -1]，所有子数组中，[3, 5, -2, 6]可以累加出最大的和12，所以返回12.
+ * 题目保证没有全为负数的数据
+ * [要求]
+ * 时间复杂度为O(n)O(n)，空间复杂度为O(1)O(1)
  */
 public class SolutionMaxsumofSubarray {
     /**
-     *
      * @param arr
      * @return
      */
-    public int maxsumofSubarray (int[] arr) {
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>0){
-                sum+=arr[i];
-            }
+    public int maxsumofSubarray(int[] arr) {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            max = sum > max ? sum : max;
+            sum = sum > 0 ? sum : 0;
         }
-        return sum;
+        return max;
     }
 
 
-    public int maxsumofSubarray2 (int[] arr) {
-        // write code here
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            if(sum+arr[i]>arr[i]){
-                sum+=arr[i];
-            } else{
-                sum=arr[i];
-            }
-        }
-
-        return sum;
-    }
 }
