9f1878c7c738f9ade6a2695fadf314712f39987f
saiding@creditease.cn <saiding@creditease.cn>
2021-10-18 18:32:29 +0800

    HttpAsyncClient

--- src/main/java/com/ding/study/net/HttpAsyncClient/Test.java
+++ src/main/java/com/ding/study/net/HttpAsyncClient/Test.java
@@ -1,6 +1,13 @@
 package com.ding.study.net.HttpAsyncClient;
 
 /**
+ *
+ * http://hc.apache.org/httpcomponents-asyncclient-4.1.x/index.html
+ *
+ * 官网
+ *
+ * http://hc.apache.org/httpcomponents-client-5.1.x/index.html
+ *
  * @author daniel 2021-10-18 0018.
  */
 public class Test {
---
+++ src/main/java/com/ding/study/net/OpenFeign/TestFeign.java
@@ -0,0 +1,11 @@
+package com.ding.study.net.OpenFeign;
+
+/**
+ *
+ * https://github.com/OpenFeign/feign
+ *
+ *
+ * @author daniel 2021-10-18 0018.
+ */
+public class TestFeign {
+}
